8f6e69a915a2c28935a4f7f22c2c117fa15610ef
Ritik Maurya <140969100+ritikmaurya4747@users.noreply.github.com>
2024-08-31 17:29:20 +0530

    All  done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -465,6 +465,8 @@ const getWatchHistory = asyncHandler(async (req, res) => {
   .json(new ApiResponse(200, user[0].watchHistory,"Watch history fetched successfully"))
 })
 
+
+
 export {
   registerUser,
   loginUser,
